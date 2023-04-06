3b53f2348ac190935923fcdb8be5b5ba7238ee0d
Victor <vitcheffvi@cy-tech.fr>
2023-04-06 15:40:51 +0200

    [temporary.py] change the code for guide 01

--- temporary.py
+++ temporary.py
@@ -1,16 +1,17 @@
-from prefect import flow, task
+# Import the necessary libraries
 import ovh
-
-from dotenv import load_dotenv
-import os
+from prefect import flow, task
 import json
 
-# Load environments variables
+import os
+from dotenv import load_dotenv
 load_dotenv(".env")
 
+# Define the task to create a client for your public Cloud.
 
+
+@task
 def init_ovh():
-    # Visit https://api.ovh.com/createToken/?GET=/me to get your credentials
     ovh_client = ovh.Client(
         endpoint=os.getenv("APP_ENDPOINT"),
         application_key=os.getenv("APP_KEY"),
@@ -19,11 +20,25 @@ def init_ovh():
     )
     return ovh_client
 
+# Define the task to get all of your notebook
 
-def get_all_notebook(client):
+
+@task
+def get_all_swift_containers(client):
     result = client.get(
-        f'/cloud/project/{os.getenv("PROJECT_ID")}/ai/notebook')
+        f'/cloud/project/{os.getenv("PROJECT_ID")}/storage')
     return (json.dumps(result, indent=4))
 
+# Define the flow to run on prefect
 
-print(get_all_notebook(init_ovh()))
+
+@flow
+def display_swift_containers():
+    # Create the OVHcloud client
+    client = init_ovh()
+    # this flow returns all of your notebook in your public cloud
+    return (get_all_swift_containers(client=client))
+
+
+# Launch your flow and display the result
+print(display_swift_containers())
