a37c96862e28d0f1a635285fe06e813167a125bf
Victor <vitcheffvi@cy-tech.fr>
2023-05-03 14:56:07 +0200

    [temporary.py] change the task names

--- temporary.py
+++ temporary.py
@@ -9,6 +9,7 @@ import os
 from dotenv import load_dotenv
 load_dotenv(".env")
 
+
 def generate_task_name():
     flow_name = flow_run.flow_name
     task_name = task_run.task_name
@@ -30,10 +31,8 @@ def generate_flow_name():
     return f"{flow_name}-with-{name}-on-{date}"
 
 
-
-
 # Define the task to create a client for your public Cloud.
-@task(name="init_client",
+@task(name="init_client-ovh",
       task_run_name=generate_task_name)
 def init_ovh(name):
     ovh_client = ovh.Client(
@@ -46,9 +45,10 @@ def init_ovh(name):
 
 # Define the task to get all of your notebook
 
+
 @task(name="get_infos",
       task_run_name=generate_task_name)
-def get_project_infos(client,project_uuid,name:str):
+def get_project_infos(client, project_uuid, name: str):
     result = client.get(
         '/cloud/project/'+str(project_uuid))
     print(json.dumps(result, indent=4))
@@ -58,13 +58,15 @@ def get_project_infos(client,project_uuid,name:str):
 
 # Define the flow to run on prefect
 @flow(flow_run_name=generate_flow_name)
-def display_project_infos(name:str):
+def display_project_infos(name: str):
 
-    project_uuid = variables.get("Project UUID", default=os.getenv("PROJECT_ID"))
+    project_uuid = variables.get(
+        "Project UUID", default=os.getenv("PROJECT_ID"))
 
-	# Create the OVHcloud client
+    # Create the OVHcloud client
     client = init_ovh(name="victor")
-	# This task print all your Public Cloud project infos
-    get_project_infos(client=client, project_uuid=project_uuid,name="victor")	
- 
+    # This task print all your Public Cloud project infos
+    get_project_infos(client=client, project_uuid=project_uuid, name="victor")
+
+
 display_project_infos(name="victor")
